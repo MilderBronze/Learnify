393739796b16625c309ae4170043477fb1c97df4
milderbronze <sumitinbki@gmail.com>
2025-08-24 17:01:53 +0530

    made a custom router matchpath wrapper to increase code reusability

--- client/src/hooks/useRouteMatch.js
+++ client/src/hooks/useRouteMatch.js
@@ -2,5 +2,22 @@ import { useLocation, matchPath } from "react-router-dom";
 
 export default function useRouteMatch(path) {
   const location = useLocation();
-  return matchPath(location.pathname, { path });
+  return matchPath(location.pathname, { path: path });
+  // return matchPath(location.pathname, { path });
 }
+/**
+ * 
+// What you have (shorthand):
+matchPath(location.pathname, { path })
+
+// Is the same as (longhand):
+matchPath(location.pathname, { path: path })
+
+// React Router's matchPath expects an options object like:
+{
+  path: "/users/:id",
+  exact: true,     // optional
+  strict: false,   // optional
+  sensitive: false // optional
+}
+ */
